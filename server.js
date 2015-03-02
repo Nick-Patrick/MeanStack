46ebdfbdb693b5ef1c1326dc135a990df8ecda50
nickpatrick <n.patrick@outlook.com>
2015-03-02 19:10:56 +0000

    Initial node and jade setup.

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
-/**
- * Created by nickpatrick on 02/03/15.
- */
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || ' development';
+
+var app = express();
+
+function compile(str, path) {
+   return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware({
+   src: __dirname + '/public',
+   compile: compile
+}));
+
+app.get('*', function (req, res) {
+   res.render('index');
+});
+app.use(express.static(__dirname + '/public'));
+
+var port = 3030;
+app.listen(port);
+
+console.log('Listening port: ' + port);
